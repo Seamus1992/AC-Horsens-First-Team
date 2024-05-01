6b5665cf3313e3badac62bbe166c6dae01432cd1
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-05-01 09:42:18 +0200

    Opdatering

--- First_team_data.py
+++ First_team_data.py
@@ -79,7 +79,6 @@ def Match_evaluation ():
     df_possession_pv_hold = df_possession_pv_hold.groupby(['team_name','label'])['PvTotal'].sum().reset_index()
 
     df_xg_hold = df_xg[df_xg['q_qualifierId'] == 321]
-    st.dataframe(df_xg)
     df_xg_hold = df_xg_hold[df_xg_hold['label'] == Kampvalg]
     df_xg_hold['q_value'] = df_xg_hold['q_value'].astype(float)
     df_xg_hold = df_xg_hold.rename(columns={'q_value': 'Open play xG'})

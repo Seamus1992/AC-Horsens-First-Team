b3fe3bb5122aac724461105cc3263e0f4ab1a985
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-05-13 11:34:39 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -90,7 +90,7 @@ def Match_evaluation ():
 
     df_pv = df_pv[df_pv['label'] == Kampvalg]
     df_xg = df_xg[df_xg['label'] == Kampvalg]
-    df_xg = df_xg[(df_xg[['9', '24', '25', '26']] != 'true').all(axis=1)]
+    #df_xg = df_xg[(df_xg[['9', '24', '25', '26']] != 'true').all(axis=1)]
     df_possession_stats = df_possession_stats[df_possession_stats['label'] == Kampvalg]
     df_possession = df_possession[df_possession['label'] == Kampvalg]
 

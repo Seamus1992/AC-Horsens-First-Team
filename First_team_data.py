f71e85c06596514446c2b6231682703e0c61c0a0
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-05-13 11:54:18 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -154,7 +154,7 @@ def Match_evaluation ():
     st.dataframe(df_holdsummary,hide_index=True)
     col1,col2,col3,col4 = st.columns(4)
 
-    cols_to_average = df_possession_stats.columns[-3:]
+    cols_to_average = df_possession_stats.columns[-5:]
     st.write(cols_to_average)
     time_column = 'interval'
 

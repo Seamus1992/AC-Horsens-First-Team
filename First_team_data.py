fdd706c1acb6a940a4b03b318b7e68406a4781af
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-05-20 08:04:59 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -1040,7 +1040,7 @@ def League_stats():
     matchstats_df['Final third entries per match'] = matchstats_df['finalThirdEntries'] / matchstats_df['matches']
     matchstats_df['Final third pass accuracy %'] = matchstats_df['successfulFinalThirdPasses'] / matchstats_df['totalFinalThirdPasses']
     matchstats_df['Open play shot assists share'] = matchstats_df['attAssistOpenplay'] / matchstats_df['totalAttAssist']
-    matchstats_df = matchstats_df.drop(columns=['contestantId','playerName','player_playerId','player_position','player_positionSide','player_subPosition'])
+    matchstats_df = matchstats_df.drop(columns=['contestantId','playerName','player_playerId','match_id','player_position','player_positionSide','player_subPosition'])
     
     cols_to_rank = matchstats_df.drop(columns=['team_name','matches','minsPlayed']).columns
     ranked_df = matchstats_df.copy()  # Create a copy of the original DataFrame

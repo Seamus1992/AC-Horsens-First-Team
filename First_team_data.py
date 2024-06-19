f1b84630d839a0df6690823c2ccf300f6739b7a6
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 15:43:33 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -237,7 +237,7 @@ def Dashboard():
         df_matchstats = load_match_stats(columns=['contestantId', 'label', 'successfulOpenPlayPass', 'openPlayPass'])
         df_possession = load_possession_data()
         xA_map = df_xA[['contestantId', 'team_name']].drop_duplicates()
-        df_matchstats = df_matchstats.merge(xA_map, on='contestantId', how='left')
+        df_matchstats = df_matchstats.merge(xA_map, on='contestantId')
         st.dataframe(df_matchstats, hide_index=True)
         df_possession = df_possession[~(df_possession[['6.0','107.0']] == True).any(axis=1)]
         df_passes_all = df_possession[df_possession['typeId'] == 1]

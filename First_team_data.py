8fd01b576d021b543bedbcfc24ba89f75e425363
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 15:35:20 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -233,9 +233,13 @@ def Dashboard():
     st.cache_data(experimental_allow_widgets=True)
     st.cache_resource(experimental_allow_widgets=True)
     def passes():
+        df_matchstats = load_match_stats()
         df_possession = load_possession_data()
+        xA_map = df_xA[['contestantId','team_name']]
+        df_matchstats = df_matchstats.merge(xA_map, on='contestantId', how='inner')
+        st.dataframe(df_matchstats, hide_index=True)
         df_possession = df_possession[~(df_possession[['6.0','107.0']] == True).any(axis=1)]
-
+        df_passes_all = df_possession[df_possession['typeId'] == 1]
         df_possession = df_possession[df_possession['label'].isin(match_choice)]
         df_passes_horsens = df_possession[df_possession['team_name'] == 'Horsens']
         df_passes_horsens = df_passes_horsens[(df_passes_horsens['typeId'] == 1) & (df_passes_horsens['outcome'] == 1)]

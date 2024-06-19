fabdd063febc316585ef8c2b6732ca489af3004a
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 10:19:24 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -117,13 +117,12 @@ def Dashboard():
 
     df_xg_summary = df_xg.groupby(['team_name','label'])['321'].sum().reset_index()
     df_xg_summary = df_xg_summary.rename(columns={'321': 'xG'})
-    st.dataframe(df_packing)
     df_packing_summary = df_packing[['team_name','label','bypassed_opponents','bypassed_defenders']]
     df_packing_summary = df_packing_summary.groupby(['team_name','label']).sum().reset_index()
-    st.dataframe(df_packing_summary)
     
     team_summary = df_xg_summary.merge(df_xA_summary, on=['team_name','label'])
     team_summary = team_summary.merge(df_passes, on=['team_name','label'])
+    team_summary = team_summary.merge(df_packing_summary, on=['team_name', 'label'])
     st.dataframe(team_summary)
 
 def Match_evaluation():

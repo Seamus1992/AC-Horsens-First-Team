2be55674f46fffb0836575440e9defc0b076834d
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 09:43:17 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -120,7 +120,7 @@ def Dashboard():
     df_xg_summary = df_xg_summary.rename(columns={'321': 'xG'})
     
     df_passes = df_matchstats[['team_name','successfulOpenPlayPass','openPlayPass']]
-    
+    df_passes = df_passes.groupby('team_name').sum().reset_index()
     
     team_summary = df_xg_summary.merge(df_xA_summary, on='team_name')
     team_summary = team_summary.merge(df_passes, on='team_name')

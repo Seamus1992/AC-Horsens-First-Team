5161ae9d63222cd95e34b6a2b9997fc736a55d5a
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 12:31:03 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -138,8 +138,8 @@ def Dashboard():
         xg_all['xG_match'] = xg_all.groupby('label')['321'].transform('sum')
         xg_all['xG difference'] = xg_all['321'] - xg_all['xG_match'] + xg_all['321']
         xg_all = xg_all.sort_values(by=['date'], ascending=True)
-        xg_all = xg_all.groupby('team_name').sum().reset_index()
-        st.dataframe(xg_all)
+        xg_all_table = xg_all.groupby('team_name').sum().reset_index()
+        st.dataframe(xg_all_table)
 
         xg_all['xG rolling average'] = xg_all.groupby('team_name')['xG difference'].transform(lambda x: x.rolling(window=3, min_periods=1).mean())
         fig = go.Figure()

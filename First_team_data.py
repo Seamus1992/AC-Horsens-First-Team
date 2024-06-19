474fc524dd6ec4c80bf4afc09008237b0879e462
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 09:18:32 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -100,7 +100,7 @@ def Dashboard():
     df_matchstats = df_matchstats[df_matchstats['label'].isin(match_choice)]
     df_possession = df_possession[df_possession['label'].isin(match_choice)]
     
-    df_xg_summary = df_xg.groupby('team_name')['321'].sum.reset_index()
+    df_xg_summary = df_xg.groupby('team_name')['321'].sum().reset_index()
     st.dataframe(df_xg_summary)
 
 def Match_evaluation():

526b539b7d19cd81aebd7f80f4a1dce1a1e02deb
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 16:01:42 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -242,7 +242,7 @@ def Dashboard():
         df_matchstats = df_matchstats.merge(xA_map, on='contestantId')
         df_matchstats = df_matchstats[['label','date', 'team_name', 'successfulOpenPlayPass', 'openPlayPass']]
         df_matchstats = df_matchstats.groupby(['label','date', 'team_name']).sum().reset_index()
-        df_matchstats_tabel = df_matchstats.groupby(['team_name']).sum().reset_index()
+        df_matchstats_tabel = df_matchstats.groupby('team_name').sum().reset_index()
         df_matchstats = df_matchstats.sort_values(by='date')
         st.dataframe(df_matchstats_tabel, hide_index=True)
         # Beregn 3-kamps rullende gennemsnit for hver team

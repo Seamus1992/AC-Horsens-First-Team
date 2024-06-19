494b68790d199a98da1f002f2897e95d93d3bf05
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 15:01:37 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -241,8 +241,8 @@ def Dashboard():
         ]
         mid_third_pass_ends = mid_third_pass_ends[['team_name','playerName','eventId', '140.0', '141.0','x', 'y','label','date']]
         mid_third_pass_ends_summary = mid_third_pass_ends.groupby(['team_name', 'playerName', 'eventId', 'label', 'date']).size().reset_index(name='count')
-        team_counts = mid_third_pass_ends_summary['team_name'].value_counts().reset_index(name='count')
-        team_counts.columns = ['team_name', 'count']
+        team_counts = mid_third_pass_ends_summary['team_name','label'].value_counts().reset_index(name='count')
+        team_counts.columns = ['team_name','label', 'count']
         player_counts = mid_third_pass_ends_summary['playerName'].value_counts().reset_index(name='count')
         player_counts.columns = ['playerName', 'count']
         col1,col2 = st.columns(2)

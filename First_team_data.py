fb8f362716eeca8ff0573e77744395ce23b29ff3
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-19 12:50:50 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -168,10 +168,12 @@ def Dashboard():
         
         st.plotly_chart(fig)
         df_xg = df_xg[~(df_xg[['24', '25', '26']] == True).any(axis=1)]
-        df_xg = df_xg.sort_values(by=['timeMin', 'timeSec'])
-        df_xg['cumulative_xG'] = df_xg.groupby(['team_name', 'label'])['321'].cumsum()
-        df_xg_avg = df_xg.groupby(['team_name', 'timeMin', 'timeSec'])['cumulative_xG'].mean().reset_index()
+        df_xg = df_xg.sort_values(by=['team_name','timeMin', 'timeSec'])
 
+        df_xg['cumulative_xG'] = df_xg.groupby(['team_name', 'label'])['321'].cumsum()
+        df_xg['team_name'] = df_xg['team_name'].apply(lambda x: x if x == 'Horsens' else 'Opponent')
+
+        df_xg_avg = df_xg.groupby(['team_name', 'timeMin', 'timeSec'])['cumulative_xG'].mean().reset_index()
         st.dataframe(df_xg_avg)
 
     

a887097c6bdd6ec451c7bcd88b7e955d1d2f3efd
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 08:35:11 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -358,7 +358,7 @@ def Dashboard():
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        selected_data1 = st.selectbox('Choose data type 1', list(Data_types.keys()),index=None)
+        selected_data1 = st.selectbox('Choose data type 1', list(Data_types.keys()))
         Data_types[selected_data1]()
 
     with col2:
@@ -467,5 +467,5 @@ Data_types = {
     'League stats': League_stats
 }
 
-selected_data = st.sidebar.radio('Choose data type',list(Data_types.keys()))
+selected_data = st.sidebar.radio('Choose data type',list(Data_types.keys()), index=0)
 Data_types[selected_data]()

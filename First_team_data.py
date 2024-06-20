64ee5a9ee5fca09a054d3ea932fb64fb87ba2e51
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 10:04:12 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -506,7 +506,7 @@ def Dashboard():
 
         st.pyplot(fig)
         st.header('Touches in zone 14')
-        df_zone14 = df_possession[(df_possession['x'].astype(float) >= 66) & (df_possession['y'].astype(float) >= 21.1) & (df_possession['y'].astype(float) <= 78.9)]
+        df_zone14 = df_possession[(df_possession['x'].astype(float) >= 66) & ((df_possession['y'].astype(float) >= 21.1) & (df_possession['y'].astype(float) <= 78.9))]
         df_zone14_team = df_zone14.groupby(['team_name', 'label']).size().reset_index(name='Touches')
         df_zone14_team = df_zone14_team.sort_values(by=['Touches'], ascending=False)
         df_zone14_player = df_zone14.groupby(['playerName']).size().reset_index(name='Touches')

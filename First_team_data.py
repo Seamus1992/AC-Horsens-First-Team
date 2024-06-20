2ff3cea54d5f9e4719cb1f17615cf89179ceda6b
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 13:17:51 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -596,15 +596,15 @@ def Dashboard():
             df_ppda = df_possession_data[df_possession_data['typeId'].isin([1, 4, 7,8, 45])]
             df_ppdabeyond40 = df_ppda[df_ppda['x'].astype(float) > 40]
             df_ppdabeyond40_passes = df_ppdabeyond40[df_ppdabeyond40['typeId'] == 1]
-            df_ppdabeyond40_passestotal = df_ppdabeyond40_passes.groupby('label')['eventId'].count().reset_index()
+            df_ppdabeyond40_passestotal = df_ppdabeyond40_passes.groupby(['label','date'])['eventId'].count().reset_index()
             df_ppdabeyond40_passestotal = df_ppdabeyond40_passestotal.rename(columns={'eventId': 'passes in game'})
-            df_ppdabeyond40_passesteams = df_ppdabeyond40_passes.groupby(['label','team_name'])['eventId'].count().reset_index()
+            df_ppdabeyond40_passesteams = df_ppdabeyond40_passes.groupby(['label','team_name','date'])['eventId'].count().reset_index()
             df_ppdabeyond40_passesteams = df_ppdabeyond40_passesteams.rename(columns={'eventId': 'passes'})
 
             df_ppdabeyond40_defactions = df_ppdabeyond40[df_ppdabeyond40['typeId'].isin([4, 7, 8, 45])]
-            df_ppdabeyond40_defactionstotal = df_ppdabeyond40_defactions.groupby('label')['eventId'].count().reset_index()
+            df_ppdabeyond40_defactionstotal = df_ppdabeyond40_defactions.groupby(['label','date'])['eventId'].count().reset_index()
             df_ppdabeyond40_defactionstotal = df_ppdabeyond40_defactionstotal.rename(columns={'eventId': 'defensive actions in game'})
-            df_ppdabeyond40_defactionsteams = df_ppdabeyond40_defactions.groupby(['label', 'team_name'])['eventId'].count().reset_index()
+            df_ppdabeyond40_defactionsteams = df_ppdabeyond40_defactions.groupby(['label', 'team_name','date'])['eventId'].count().reset_index()
             df_ppdabeyond40_defactionsteams = df_ppdabeyond40_defactionsteams.rename(columns={'eventId': 'defensive actions'})
             df_ppdabeyond40total = df_ppdabeyond40_defactionstotal.merge(df_ppdabeyond40_passestotal)
             df_ppdabeyond40 = df_ppdabeyond40_defactionsteams.merge(df_ppdabeyond40total)

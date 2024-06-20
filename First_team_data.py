da090ab050e6c4b8e4f237cc705685c7b3abe37e
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 11:04:30 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -398,7 +398,7 @@ def Dashboard():
     def packing():
         df_packing = load_packing_data()
         df_packing = df_packing[df_packing['pass_receiver'] != '']
-        df_packing = df_packing[df_packing['pass_receiver'] != None]
+        df_packing = df_packing[df_packing['pass_receiver'].dropna()]
         st.dataframe(df_packing, hide_index=True)
         packing_teams = df_packing.groupby('team_name')[['bypassed_opponents','bypassed_defenders']].sum().reset_index()
         packing_teams = packing_teams.sort_values(by='bypassed_opponents', ascending=False)

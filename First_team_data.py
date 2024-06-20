985cc95e507f46b3019089149494c6579073e399
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 13:46:14 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -624,12 +624,20 @@ def Dashboard():
         df_ppda = calculate_ppda(df_possession_data)
         df_ppda = df_ppda[df_ppda['team_name'] == 'Horsens']
         df_ppda_season_average = df_ppda.groupby(['label'])['PPDA'].mean().reset_index()
+        df_ppda_season_average = df_ppda_season_average.sort_values(by=['label'], ascending=True)
+
+        # Ensure sorting by date for display
+        df_ppda_sorted = df_ppda.sort_values(by=['date', 'label'], ascending=[True, True])
+
         st.header('Whole season')
-        st.bar_chart(df_ppda[['label', 'PPDA']].set_index('label'))
-        st.dataframe(df_ppda, hide_index=True)
-        
+        st.bar_chart(df_ppda_sorted[['label', 'PPDA']].set_index('label'))
+        st.dataframe(df_ppda_sorted, hide_index=True)
+
+
         st.header('Chosen matches')
-        df_ppda_chosen_period = df_ppda[df_ppda['label'].isin(match_choice)]
+        df_ppda_chosen_period = df_ppda_sorted[df_ppda_sorted['label'].isin(match_choice)]
+        st.bar_chart(df_ppda_chosen_period[['label', 'PPDA']].set_index('label'))
+        st.dataframe(df_ppda_chosen_period, hide_index=True)
         
     Data_types = {
         'xG': xg,

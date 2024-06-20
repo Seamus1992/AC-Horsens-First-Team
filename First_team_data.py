e65c96c34e02623006a9f694746287326ce61219
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 08:14:31 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -329,7 +329,7 @@ def Dashboard():
         team_counts = team_counts.sort_values(by=['count'], ascending=False)
 
         # Tæl forekomster af hver playerName
-        player_counts = mid_third_pass_ends['playerName','pass_receiver'].value_counts().reset_index(name='count')
+        player_counts = mid_third_pass_ends[('playerName','pass_receiver')].value_counts().reset_index(name='count')
         player_counts.columns = ['playerName','pass_receiver', 'count']
         st.write('Passes from side to halfspace/centerspace')
         st.dataframe(player_counts,hide_index=True)

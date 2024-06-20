b1d9b1669a8ac86b8728949f2c72b89625eef72f
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 11:50:37 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -722,10 +722,37 @@ Data_types = {
     'League stats': League_stats
 }
 
-st.cache_data(experimental_allow_widgets=True)
-st.cache_resource(experimental_allow_widgets=True)
-selected_data = st.sidebar.radio('Choose data type',list(Data_types.keys()))
+# Initialize the session state if not already done
+if 'selected_data1' not in st.session_state:
+    st.session_state['selected_data1'] = ''
+if 'selected_data2' not in st.session_state:
+    st.session_state['selected_data2'] = ''
+if 'selected_data3' not in st.session_state:
+    st.session_state['selected_data3'] = ''
 
-st.cache_data(experimental_allow_widgets=True)
-st.cache_resource(experimental_allow_widgets=True)
-Data_types[selected_data]()
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    selected_data1 = st.selectbox('Choose data type 1', [''] + list(Data_types.keys()), key='selected_data1_selectbox')
+    if selected_data1 and selected_data1 != st.session_state['selected_data1']:
+        st.session_state['selected_data1'] = selected_data1
+
+with col2:
+    selected_data2 = st.selectbox('Choose data type 2', [''] + list(Data_types.keys()), key='selected_data2_selectbox')
+    if selected_data2 and selected_data2 != st.session_state['selected_data2']:
+        st.session_state['selected_data2'] = selected_data2
+
+with col3:
+    selected_data3 = st.selectbox('Choose data type 3', [''] + list(Data_types.keys()), key='selected_data3_selectbox')
+    if selected_data3 and selected_data3 != st.session_state['selected_data3']:
+        st.session_state['selected_data3'] = selected_data3
+
+# Display the current selection results
+if st.session_state['selected_data1']:
+    Data_types[st.session_state['selected_data1']]()
+
+if st.session_state['selected_data2']:
+    Data_types[st.session_state['selected_data2']]()
+
+if st.session_state['selected_data3']:
+    Data_types[st.session_state['selected_data3']]()

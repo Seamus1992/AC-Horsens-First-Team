066b5e3b3a6ff67015e45cf3419042d5983c69df
Seamus1992 <130638822+Seamus1992@users.noreply.github.com>
2024-06-20 11:52:33 +0200

    Update First_team_data.py

--- First_team_data.py
+++ First_team_data.py
@@ -730,22 +730,36 @@ if 'selected_data2' not in st.session_state:
 if 'selected_data3' not in st.session_state:
     st.session_state['selected_data3'] = ''
 
+# Sidebar for selecting data types
+st.sidebar.title('Select Data Types')
+selected_data1 = st.sidebar.selectbox('Choose data type 1', [''] + list(Data_types.keys()), key='selected_data1_selectbox')
+selected_data2 = st.sidebar.selectbox('Choose data type 2', [''] + list(Data_types.keys()), key='selected_data2_selectbox')
+selected_data3 = st.sidebar.selectbox('Choose data type 3', [''] + list(Data_types.keys()), key='selected_data3_selectbox')
+
+# Update session state and run functions if selections change
+if selected_data1 and selected_data1 != st.session_state['selected_data1']:
+    st.session_state['selected_data1'] = selected_data1
+
+if selected_data2 and selected_data2 != st.session_state['selected_data2']:
+    st.session_state['selected_data2'] = selected_data2
+
+if selected_data3 and selected_data3 != st.session_state['selected_data3']:
+    st.session_state['selected_data3'] = selected_data3
+
+# Columns to display the results
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    selected_data1 = st.selectbox('Choose data type 1', [''] + list(Data_types.keys()), key='selected_data1_selectbox')
-    if selected_data1 and selected_data1 != st.session_state['selected_data1']:
-        st.session_state['selected_data1'] = selected_data1
+    if st.session_state['selected_data1']:
+        Data_types[st.session_state['selected_data1']]()
 
 with col2:
-    selected_data2 = st.selectbox('Choose data type 2', [''] + list(Data_types.keys()), key='selected_data2_selectbox')
-    if selected_data2 and selected_data2 != st.session_state['selected_data2']:
-        st.session_state['selected_data2'] = selected_data2
+    if st.session_state['selected_data2']:
+        Data_types[st.session_state['selected_data2']]()
 
 with col3:
-    selected_data3 = st.selectbox('Choose data type 3', [''] + list(Data_types.keys()), key='selected_data3_selectbox')
-    if selected_data3 and selected_data3 != st.session_state['selected_data3']:
-        st.session_state['selected_data3'] = selected_data3
+    if st.session_state['selected_data3']:
+        Data_types[st.session_state['selected_data3']]()
 
 # Display the current selection results
 if st.session_state['selected_data1']:
